15e5be11fc7877c7d94dba2cb207ca04c60fca95
Daniel Worthington <daniel@halffullheart.com>
2022-09-30 14:53:33 -0700

    Devigner setup

---
+++ README_DEVIGN.md
@@ -0,0 +1,7 @@
+# Devign Setup
+
+1. Check out the repo.
+2. `yarn`
+3. `yarn link devigner`
+4. `yarn link babel-plugin-devign`
+5. Start via runner: `./runner.sh -u ../user-servers/netflix`
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
+import * as Devign from 'devigner';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
 import { auth } from './lib/auth';
 import { AuthContext } from './context/auth';
-// import { init } from 'devigner';
 
-// init();
+Devign.init();
 
 render(
   <React.StrictMode>
