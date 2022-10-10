30b0ec02bd5c13f8cfb15e0c69bb202b54cc8c3b
Daniel Worthington <daniel@halffullheart.com>
2022-10-10 12:16:45 -0700

    A different header for each profile

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -77,7 +77,7 @@ export function BrowseContainer({ slides }) {
     <>
       {loading ? <Loading src={profile.avatar} /> : <LoadingReleaseBody />}
 
-      <Header src="joker1" dontShowOnSmallViewPort>
+      <Header src={profile.header.image} dontShowOnSmallViewPort>
         <HeaderFrame>
           <HeaderGroup>
             <HeaderLogo to={ROUTES.HOME} src={logo} alt="Netflix" />
@@ -108,12 +108,8 @@ export function BrowseContainer({ slides }) {
         </HeaderFrame>
 
         <HeaderFeature>
-          <HeaderFeatureCallOut>Watch Joker Now</HeaderFeatureCallOut>
-          <HeaderText>
-            Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
-            City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
-            futile attempt to feel like he's part of the world around him.
-          </HeaderText>
+          <HeaderFeatureCallOut>{profile.header.title}</HeaderFeatureCallOut>
+          <HeaderText>{profile.header.description}</HeaderText>
           <HeaderPlayButton>Play</HeaderPlayButton>
         </HeaderFeature>
       </Header>
